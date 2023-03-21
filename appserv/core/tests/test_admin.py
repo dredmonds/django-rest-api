c450e18d286b293653b9d0380282bb6b4e37f34f
Edmond De Los Reyes <dredmonds@gmail.com>
2023-03-21 15:38:52 +0000

    Configure to make Django admin users list for adding

--- appserv/core/tests/test_admin.py
+++ appserv/core/tests/test_admin.py
@@ -4,7 +4,6 @@ Tests for the Django admin modification.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from django.test import Client
 
 
 class AdminSiteTests(TestCase):
@@ -34,6 +33,13 @@ class AdminSiteTests(TestCase):
     def test_edit_user_page(self):
         """Test the edit user page works."""
         url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
